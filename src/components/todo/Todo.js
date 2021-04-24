f7198e0dbc7a241c2793073c689b170e52464e6e
denzord <45736463+denzord@users.noreply.github.com>
2021-04-24 23:10:01 +0700

    Update Todo.js

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -1,11 +1,24 @@
-import TodoList from "../todo-list/TodoList";
 import "./Todo.css";
+import { useState, usestate } from "react";
+import TodoCreate from "../todo-create/TodoCreate";
+import TodoList from "../todo-list/TodoList";
 
 const Todo = () => {
+  const [getTodos, setTodos] = useState([
+    { id: 1, title: "Eat" },
+    { id: 2, title: "Code" },
+    { id: 3, title: "Sleep" },
+  ]);
+
+  const eventCreateTodo = (todo) => {
+    setTodos(getTodos.concat(todo));
+    console.log(getTodos);
+  };
   return (
     <div>
       <h3>Todo List</h3>
-      <TodoList />
+      <TodoCreate onCreateTodo={eventCreateTodo} />
+      <TodoList dataTodos={getTodos} />
     </div>
   );
 };
